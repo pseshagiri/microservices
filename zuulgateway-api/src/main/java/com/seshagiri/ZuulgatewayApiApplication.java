136a14bb71d1fb199ae82413c3e1abb4582e7f75
pseshagiri <spichika@gmail.com>
2021-05-22 13:05:18 +0530

    Add files via upload
    commiting files

---
+++ zuulgateway-api/src/main/java/com/seshagiri/ZuulgatewayApiApplication.java
@@ -0,0 +1,13 @@
+package com.seshagiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulgatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulgatewayApiApplication.class, args);
+	}
+
+}
